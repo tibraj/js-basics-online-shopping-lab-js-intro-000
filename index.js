500b31b6e7cfef9f9a3e2f99231cb94c03f3ce03
Taulant Ibraj <tibraj94@gmail.com>
2020-02-23 00:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,9 +47,10 @@ function removeFromCart(item) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
-    } else {
+    } 
+  }
+  if (item !== cart[item]) {
       return "That item is not in your cart."
-    }
   }
 }
 
